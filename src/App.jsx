9ae0d0735352d614c3bfdced8b3d4380d6b41850
Charles M Dellinger <charles.dellinger@gmail.com>
2022-01-05 03:09:16 +0000

    added GIF link entry box to webpage

--- src/App.jsx
+++ src/App.jsx
@@ -87,6 +87,15 @@ const App = () => {
    */
   const renderConnectedContainer = () => (
     <div className="connected-container">
+        {/* Input and button for submitting pictures */}
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+          }}
+        >
+          <input type="text" placeholder="Enter gif link!" />
+          <button type="submit" className="cta-button submit-gif-button">Submit</button>
+        </form>
       <div className="gif-grid">
         {TEST_GIFS.map(gif => (
           <div className="gif-item" key={gif}>
